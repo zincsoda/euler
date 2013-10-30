abd48cbbdd94215454c8cdeb7180c38b26d93b82
Steve Walsh <steve.walsh@sap.com>
2013-10-30 12:37:05 +0000

    some update

--- p31.py
+++ p31.py
@@ -1,4 +1,29 @@
 #!/usr/bin/python2.6
 
+coins = [1,2,5,10,20,50,100,200]
+
+'''
+single type:
+    1p coin
+    2p coin
+
+two types:
+    1p coin:
+        with a 2p, lots of ways
+        with 5p, lots of ways
+        with ...
+        with 200p, 1 way
+
+three types:
+    1p coin:
+        with 2p:
+            with 5p: ..
+            with 10p: ..
+            ..
+            with 200p: ..
+        with 5p:
+'''
+
+
 if __name__=="__main__":
     print 'hey'
